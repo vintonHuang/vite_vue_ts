17549d03b2fccc00f4be689ef56a5e41053800ab
huangwentao <huangwentao@dianchu.com>
2022-03-04 22:40:31 +0800

    修改vite.config.j配置文件

--- vite.config.ts
+++ vite.config.ts
@@ -29,4 +29,11 @@ export default defineConfig({
       },
     },
   },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: '@import "@//styles/main.scss";',
+      },
+    },
+  },
 });
