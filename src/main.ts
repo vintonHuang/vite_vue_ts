cc197b1902ab0c81fa71af764cfb430255103b70
huangwentao <huangwentao@dianchu.com>
2022-03-04 21:41:34 +0800

    提交测试

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,9 @@
+/*
+ * @Author: Vinton
+ * @Date: 2022-03-04 21:17:53
+ * @Description: file content
+ */
 import { createApp } from "vue";
 import App from "./App.vue";
-
+console.log('main.ts')
 createApp(App).mount("#app");
