fed0ec484df4bcc7be725d108946323deddbcd03
huangwentao <huangwentao@dianchu.com>
2022-03-04 22:11:55 +0800

    提交信息测试

---
+++ src/main.js
@@ -0,0 +1,9 @@
+/*
+ * @Author: Vinton
+ * @Date: 2022-03-04 21:17:53
+ * @Description: file content
+ */
+import { createApp } from "vue";
+import App from "./App.vue";
+createApp(App).mount("#app");
+//# sourceMappingURL=main.js.map
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
+/*
+ * @Author: Vinton
+ * @Date: 2022-03-04 21:17:53
+ * @Description: file content
+ */
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-
+import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
 });
