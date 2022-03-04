a40e5916cadc2d6380cedcc8c8ec85e3628fc4b5
huangwentao <huangwentao@dianchu.com>
2022-03-04 22:37:57 +0800

    vite2.0 + vue3.x + ts全家桶搭建完成

--- vite.config.ts
+++ vite.config.ts
@@ -14,4 +14,19 @@ export default defineConfig({
       "@": path.resolve(__dirname, "src"),
     },
   },
+  server: {
+    host: "0.0.0.0",
+    port: 3000,
+    open: true,
+    https: false,
+    proxy: {},
+  },
+  build: {
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+      },
+    },
+  },
 });
